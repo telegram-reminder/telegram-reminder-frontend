e8463974b7c44f523e03a6dac4eacb4ef36c8fb7
telegram-reminder <axel.reminder.bot@email.com>
2026-01-25 14:42:56 +0100

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:8787"
-    }
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
-});
+})
