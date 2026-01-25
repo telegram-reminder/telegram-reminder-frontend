b8a657de97ba4a5b869e8a0a35759a464a972a27
telegram-reminder <axel.reminder.bot@email.com>
2026-01-25 16:54:00 +0100

    DEBUG: alert in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
+alert("MAIN.JSX ESEGUITO");
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+console.log("MAIN.JSX CONSOLE LOG");
+
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => {
+    regs.forEach(reg => reg.unregister());
+  });
+}
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
