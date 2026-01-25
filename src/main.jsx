4f48ce7a85020880b8b4bf0ea972cb7798f4a40c
telegram-reminder <axel.reminder.bot@email.com>
2026-01-25 18:10:32 +0100

    chore: remove debug alert from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,8 @@
-alert("MAIN.JSX ESEGUITO");
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-console.log("MAIN.JSX CONSOLE LOG");
 
-
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then(regs => {
-    regs.forEach(reg => reg.unregister());
-  });
-}
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
